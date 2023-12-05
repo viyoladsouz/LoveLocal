3f05c0f42c23677edbf7ccd15ccfb80bd99cf5dc
Viyola Henna Dsouza <106856582+viyoladsouz@users.noreply.github.com>
2023-12-05 19:40:23 +0530

    Update Medium_occurance.py

--- Medium_occurance.py
+++ Medium_occurance.py
@@ -1,16 +1,16 @@
-listOfNo=[1]        #taking input
+listOfNo=[1,2,3,2,3]        #taking input
 def occurance(lists):
     dict={i:0 for i in lists}       #creating dictionary and initializing all item in the list to 0
     for i in lists:
         dict[i]+=1      #counting number of occurance of each number
-    max=dict[1]         #
-    for i in dict:
+    max=dict[1]         #initializing max to 1st item
+    for i in dict: 
         if dict[i]>max:
-            max=dict[i]
+            max=dict[i]     #finding maximum occurance
     out=[]
     for i in dict:
-        if dict[i]==max:
-            out.append(i)
+        if dict[i]==max:        
+            out.append(i)       #adding numbers with maximum occurance to output list
     return out
 result=occurance(listOfNo)
 print(result)
